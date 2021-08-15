00c32eab2520fbc098f91f821097b6e4747d874b
이우주 <wown387@naver.com>
2021-08-15 14:36:06 +0900

    next js - style component

--- cooker/src/components/Header.tsx
+++ cooker/src/components/Header.tsx
@@ -2,14 +2,24 @@ import React from 'react'
 import Nav from './Nav'
 import { IMAGES } from 'src/commons/variables'
 import Image from 'next/image'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: red;
+`;
+
 export default function Header() {
-    console.log(IMAGES.LOGO);
 
     return (
         <div>
-            <Image src={IMAGES.LOGO} alt="Picture of the author" />
-            
-            <Nav />
+            <Title>
+                <Image src={IMAGES.LOGO} alt="Picture of the author" />
+                <Nav />
+                
+                asdfsf
+            </Title>
         </div>
     )
 }
