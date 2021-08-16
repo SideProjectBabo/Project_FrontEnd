04150d5594e9c1dfb0a7be708ab77ce6db07ff78
이우주 <wown387@naver.com>
2021-08-16 19:45:03 +0900

    init nav component

--- cooker/src/components/Nav.tsx
+++ cooker/src/components/Nav.tsx
@@ -1,9 +1,45 @@
 import React from 'react'
-
+import styled from 'styled-components'
 export default function Nav() {
+
+    const NavWrapper = styled.div`
+        padding: 30px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        .nav{
+            font-size: 15px;
+            display: flex;
+            flex-direction: column;
+        }
+        .select{
+            width: 100%;
+            height: 2px;
+            background-color: #FB9620;
+        }
+        .selected{
+            color:#FB9620;
+        }
+    `
     return (
-        <div>
-            Nav
-        </div>
+        <NavWrapper>
+            <div className="nav">
+                <span className="selected" >레시피</span>
+                <span className="select"></span>
+            </div>
+            <div>
+                <span>제철레시피</span>
+            </div>
+            <div>
+                <span>요리팁</span>
+            </div>
+            <div>
+                <span>커뮤니티</span>
+            </div>
+            <div>
+                <span>쿠커소식</span>
+            </div>
+
+        </NavWrapper>
     )
 }
